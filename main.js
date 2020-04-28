e480a68c40088533429ff68d1b32668c185c4f4a
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-04-28 16:30:17 +0200

    esercizio base completato

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
 $(document).ready(function() {
-	//Code
+	
+	var playlist = $(".cds-container")
+
+	//handlebars
+  var source = $("#disk-template").html();
+  //console.log(source);
+  var template = Handlebars.compile(source);
+	
+
+
+	$.ajax({
+		url: "https://flynn.boolean.careers/exercises/api/array/music",
+		method: "GET",
+		success: function (data) {
+
+			var dati = data.response;
+
+			for(var i = 0; i < 10   /*o data.response.length*/; i++) {
+
+				var disco = {
+					poster: dati[i].poster,
+					title: dati[i].title,
+					author:dati[i].author,
+					year: dati[i].year,
+				}
+				var html = template(disco);
+				playlist.append(html)
+
+			}
+
+		},
+		error: function (){
+			console.log("Errore chiamata API");
+		}
+	});
+
+
+
+
+
+
+
+
+	
 });
